83356b4c6a2f0b7287e134110c11ea5f96c1c703
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-07 00:40:28 +0300

    refactor the code

--- script.js
+++ script.js
@@ -11,23 +11,24 @@ function check(){
     var year = document.getElementById('year').value;
     var cc =parseInt(year.slice(0,2));
     var yy = parseInt(year.slice(2,4));
-    var yr = parseInt(year)
+    var yr = parseInt(yr)
 
-    var dayWeek = parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7;
+    var dayWeek = parseInt(((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7;
     
     
-    if (dd < 1 || dd > 31 || dd ==""){
+    if (dd <= 0 || dd > 31){
         alert("Invalid Date")
-    } else if (mm <= 0 || mm > 12  || mm == 2 && dd > 29 || mm ==""){
+    } else if (mm <= 0 || mm > 12  || mm == 2 && dd > 29){
         alert("Invalid Month");
-    } else if (yr <= 0 || yr > 2022 || yr ==""){
+    } 
+    else if (yr <= 0 || yr > 2022){
         alert("Invalid Year")
     } 
 
-    else if (gender === 'male'){
-        alert('Your akaname is ' + maleNames[dayWeek])
+    if (gender === 'male'){
+        alert('your Akaname is ' + maleNames[dayWeek])
     } else if (gender === 'female'){
-        alert('Your akaname is ' + femaleNames[dayWeek])
+        alert('Your Akaname is ' + femaleNames[dayWeek])
     } 
 }
 function refreshPage(){
