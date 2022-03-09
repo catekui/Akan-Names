5dc87f5414d75505d85451f5e115721235c84b03
catekui <catherine.wanjiku@student.moringaschool.com>
2022-03-09 15:53:34 +0300

    refactor the code

--- script.js
+++ script.js
@@ -9,27 +9,39 @@ function check(){
     var month = document.getElementById('month').value;
     var mm = parseInt(month);
     var year = document.getElementById('year').value;
-    var cc =parseInt(year.slice(0,2));
+    var cc = parseInt(year.slice(0,2));
     var yy = parseInt(year.slice(2,4));
-    var yr = parseInt(yr)
+    var yr = parseInt(year);
 
-    var dayWeek = Math.abs(parseInt(((cc/4) - 2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7);
+    var birthday = new Date (yr+"/" + mm +"/" + dd) 
+    var dayWeek = birthday.getDay()
     
-    
-    if (dd <= 0 || dd > 31){
-        alert("Invalid Date")
-    } else if (mm <= 0 || mm > 12  || mm == 2 && dd > 29){
-        alert("Invalid Month");
-    } 
-    else if (yr <= 0 || yr > 2022){
-        alert("Invalid Year")
-    } 
+    if (day === "" ){
+        alert("Please enter the day details");
 
-    if (gender === 'male'){
-        alert('your Akaname is ' + maleNames[dayWeek])
-    } else if (gender === 'female'){
-        alert('Your Akaname is ' + femaleNames[dayWeek])
     } 
+    else if (month === ""){
+        alert("Please enter the month details")
+    }
+    else if (year === ""){
+        alert("Please enter the year details")
+    }    
+    else {
+
+        if (dd <= 0 || dd > 31){
+            alert("Invalid Date")
+        } else if (mm <= 0 || mm > 12  || mm == 2 && dd > 29){
+            alert("Invalid Month");
+        } 
+        else if (yr <= 0 || yr > 2022){
+            alert("Invalid Year")
+        } 
+        if (gender === 'male'){
+            alert('Your Aka Name is ' + maleNames[dayWeek])
+        } else if (gender === 'female'){
+            alert('Your Aka Name is ' + femaleNames[dayWeek])
+        } 
+    }
 }
 function refreshPage(){
     window.location.reload();
